f8dfd8ceeafa6c1329536bdec0d2be6e725f4d07
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-10-05 00:04:57 -0300

    iniciando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) =>{
-  return response.json({message: "Hello World"})
-})
+app.use(express.json());
 
-app.post("/courses", (request, response) =>{
-  const {name}= request.body;
-
-  return response.json({name})
-})
-
-app.listen(3333, ()=>{ console.log("Server is running")})
-
+app.listen(3333, () => {
+  console.log("Server is running");
+});
