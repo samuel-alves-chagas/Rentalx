18fd989d18503aec840328c55c12afdcc83e797e
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-10-24 23:11:36 -0300

    Refatorando a listagem de categorias

--- src/routes/caregories.routes.ts
+++ src/routes/caregories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../module/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../module/cars/useCases/createCategory";
+import { listCategoriesController } from "../module/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository(); // Instanciando a entidade de manipulação de dados
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.json(all);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
